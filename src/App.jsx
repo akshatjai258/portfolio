4d218750dc15fd7c1385f01e011207db66ff004d
akshatjai258 <akshatjai258@gmail.com>
2024-04-21 20:07:02 +0530

    made basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-const App= () => {
 
+import { BrowserRouter } from "react-router-dom"
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works} from './components';
+import { StarsCanvas } from "./components/canvas";
+const App= () => {
   return (
-    <div>3d Dev Porfolio</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
